bb3ff1b8f94d167a246551644bd68d6a18d88010
Artur Ermolenko <artur.e@mate.academy>
2024-06-21 09:06:00 +0300

    Updates

---
+++ _next/static/chunks/app/layout-dfbfb19e98dfef5e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{9033:function(n,e,u){Promise.resolve().then(u.t.bind(u,1385,23))},1385:function(){}},function(n){n.O(0,[291,971,23,744],function(){return n(n.s=9033)}),_N_E=n.O()}]);
